24572b76163cbc3e00098794e73f3d2ff2197412
Blessen25 <blessenvinoymathew25@gmail.com>
2025-01-14 10:30:30 +0530

    Commited Changes

--- frontend/shoe_website/src/screens/main_screen.tsx
+++ frontend/shoe_website/src/screens/main_screen.tsx
@@ -3,6 +3,7 @@ import Api_component from '../components/apicomponents/api.tsx';
 import Home from '../components/home/home.tsx';
 import { Container_cstm } from '../components/extra/extra.tsx';
 import Header_comp from '../components/header/header.tsx';
+import './main_screen.css';
 
 const Main_component = () => {
     return (
