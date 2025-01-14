86cce64a01855846c177e2261f1027dcdff99269
Blessen25 <blessenvinoymathew25@gmail.com>
2025-01-14 10:20:41 +0530

    Commited Changes

--- frontend/shoe_website/src/screens/main_screen.tsx
+++ frontend/shoe_website/src/screens/main_screen.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import Api_component from '../components/apicomponents/api.tsx';
-import Home from '../components/Home/home.tsx';
+import Home from '../components/home/home.tsx';
 import { Container_cstm } from '../components/extra/extra.tsx';
 import Header_comp from '../components/header/header.tsx';
 
 const Main_component = () => {
     return (
         <>
-        <Header_comp />
-        <Home />
+            <Header_comp />
+            <Home />
         </>
     )
 }
