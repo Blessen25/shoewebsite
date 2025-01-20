adceb773cb178e06574ebfe9a1a2d6a3e361232f
Blessen25 <blessenvinoymathew25@gmail.com>
2025-01-20 10:21:10 +0530

    Commited Changes

--- frontend/shoe_website/src/components/header/header.tsx
+++ frontend/shoe_website/src/components/header/header.tsx
@@ -6,10 +6,15 @@ const Header_comp = () => {
         <>
             <section id='Header'>
                 <div className="headerchildone">
-
+                    <div className="logodiv">
+                        <img src="../assets/images/logo/QuickShoe(Light).png" alt="" className='Logoimg' />
+                    </div>
                 </div>
                 <div className="headerchildtwo">
-
+                    <a href="#">Home</a>
+                    <a href="#">Shoe</a>
+                    <a href="#">About</a>
+                    <a href="#">Collection</a>
                 </div>
                 <div className="headerchildthree">
 
