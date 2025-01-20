6852d0880ed91c52ac2dc9d2fe4f9baac8789365
Blessen25 <blessenvinoymathew25@gmail.com>
2025-01-20 17:23:03 +0530

    Merged with parent

---
+++ frontend/shoe_website/src/components/buttons/buttoncomponents.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import './buttoncomponents.css'
+
+export const ButtonTextOnly = () => {
+    return (
+        <h1>Booking</h1>
+    )
+}
+
---
+++ frontend/shoe_website/src/models/interface.tsx
@@ -0,0 +1,7 @@
+import { CSSProperties } from 'react';
+
+export interface ButtonTextOnlyProps {
+    text?: string;
+    stylecstm?: CSSProperties;
+
+}
