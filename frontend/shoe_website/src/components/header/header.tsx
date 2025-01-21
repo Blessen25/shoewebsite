f13229565ff1abcd03a7d0c3c376cfa85ffce6d4
Blessen 25 <blessenvinoymathew25@gmail.com>
2025-01-21 22:29:25 +0530

    Commited Changes

--- frontend/shoe_website/src/components/header/header.tsx
+++ frontend/shoe_website/src/components/header/header.tsx
@@ -1,20 +1,29 @@
 import React, { useState } from 'react';
 import './header.css'
 import { ButtontextandIcon, ButtonTextOnly } from '../buttons/buttoncomponents.tsx';
-import { FaUserAlt } from 'react-icons/fa';
+import { FaBars, FaLink, FaUserAlt } from 'react-icons/fa';
 
 const Header_comp = () => {
 
     const [lightmode, setLightMode] = useState(true);
+    const [activeslider, setactiveSlider] = useState(false);
 
     const handlelightmodechange = () => {
         setLightMode((prevval) => !prevval)
+    }
+
+    const handleactiveSlider = () =>{
+        setactiveSlider((prevval) => !prevval)
+        console.log(activeslider)
     }
 
 
     return (
         <>
             <section id='Header'>
+                <div className="headerchildmenulink" onClick={handleactiveSlider}>
+                    <FaBars/>
+                </div>
                 <div className="headerchildone">
                     <div className="logodiv">
                         <img src="../assets/images/logo/QuickShoe.png" alt="Logoimg" className='Logoimg' />
@@ -27,8 +36,8 @@ const Header_comp = () => {
                     <a href="#">Collection</a>
                 </div>
                 <div className="headerchildthree">
-                    {/* <ButtonTextOnly link={true} text='Login' fullwidth={false} /> */}
-                    <ButtontextandIcon link={false} text='Login' fullwidth={false} route='#' icon={<FaUserAlt />} />
+                    <ButtonTextOnly link={true} text='Login' fullwidth={false} />
+                    {/* <ButtontextandIcon link={false} text='Login' fullwidth={false} route='#' icon={<FaUserAlt />} /> */}
                     <div className={`${lightmode ? ('lightmodediv') : ('darkmode')}`}>
                         <div className={`${lightmode ? ('innerlightmodecircle') : ('innerdarkmodecircle')}`} onClick={handlelightmodechange}>
 
